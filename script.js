9baed30efa12eb629835e40dedc77d8c5674e429
b-rajshekar <91706770+b-rajshekar@users.noreply.github.com>
2022-08-05 22:39:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+const quoteText = document.querySelector(".quote"),
+authorName = document.querySelector(".author .name"),
+quoteBtn = document.querySelector("button"),
+soundBtn = document.querySelector(".sound"),
+copyBtn = document.querySelector(".copy"),
+twitterBtn = document.querySelector(".twitter");
 
+//random quote function
+function randomQuote() {
+    //loading quote
+    quoteBtn.innerHTML = "Loading...";
+    //fetching random quotes/data from the API and parsing it into JavaScript object
+    fetch("https://api.quotable.io/random").then(res => res.json()).then(result =>{
+        console.log(result);
+        quoteText.innerHTML = result.content;
+        authorName.innerHTML = result.author;
+        quoteBtn.innerHTML = "New Quote";
+    });
+}
+
+soundBtn.addEventListener("click", ()=> {
+    //the SpeechSynthesisUtterance is a web speech api that represents a speech request
+    let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerHTML}`);
+    speechSynthesis.speak(utterance); //speak method of speechSynthesis speaks the utternance
+});
+
+copyBtn.addEventListener("click", ()=> {
+    //copying the quote text on copyBtn click
+    //writeText() property writes the specified text string to the system clipboard.
+    navigator.clipboard.writeText(quoteText.innerText);
+});
+
+quoteBtn.addEventListener("click", randomQuote);
